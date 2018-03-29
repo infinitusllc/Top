ecda7d3f4af555231e7a29e60a1fc43081845e72
Alexander Nodia <alekonodia@gmail.com>
2018-03-29 21:11:57 +0400

    home

--- mods/button_links.mod.php
+++ mods/button_links.mod.php
@@ -1,6 +1,4 @@
-<form action="mail.php">
-    <button style="position: fixed; top: 91%; left: 90%; background-color: #67498c; color: white; padding: 10px; border-radius: 50%; z-index: 120" > მეილი </button>
-</form>
-<form action="reviews.php">
-    <button style="position: fixed; top: 91%; left: 85%; background-color: #67498c; color: white; padding: 10px; border-radius: 50%; z-index: 120"> რევიუ </button>
-</form>
+    <span href="mail.php" id="ui-to-top1" class="ui-to-top1 fa material-icons-chat active"></span>
+	<!-- <button style="position: fixed; top: 91%; left: 90%; background-color: #67498c; color: white; padding: 10px; border-radius: 50%; z-index: 120" > მეილი </button> -->
+	<span href="reviews.php" id="ui-to-top1" class="ui-to-top2 fa material-icons-rate_review active"></span>
+    <!-- <button style="position: fixed; top: 91%; left: 85%; background-color: #67498c; color: white; padding: 10px; border-radius: 50%; z-index: 120"> რევიუ </button> -->
