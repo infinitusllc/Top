87bf0832f06ed804a60240d710ca816c30ff929a
tura89 <turdzeladze@gmail.com>
2018-03-29 21:18:25 +0400

    button links and header

--- mods/button_links.mod.php
+++ mods/button_links.mod.php
@@ -1,4 +1,4 @@
-    <span href="mail.php" id="ui-to-top1" class="ui-to-top1 fa material-icons-chat active"></span>
+    <a href="mail.php"> <span id="ui-to-top1" class="ui-to-top1 fa material-icons-chat active"></span> </a>
 	<!-- <button style="position: fixed; top: 91%; left: 90%; background-color: #67498c; color: white; padding: 10px; border-radius: 50%; z-index: 120" > მეილი </button> -->
-	<span href="reviews.php" id="ui-to-top1" class="ui-to-top2 fa material-icons-rate_review active"></span>
+    <a href="reviews.php"> <span id="ui-to-top1" class="ui-to-top2 fa material-icons-rate_review active"></span> </a>
     <!-- <button style="position: fixed; top: 91%; left: 85%; background-color: #67498c; color: white; padding: 10px; border-radius: 50%; z-index: 120"> რევიუ </button> -->
