4d4069b084204ad1491aa879fea5d7a1865288cb
tura89 <turdzeladze@gmail.com>
2018-04-03 19:53:55 +0400

    map is now interactive.

--- mods/footer.mod.php
+++ mods/footer.mod.php
@@ -1,8 +1,6 @@
 <?php include "includes/get_generics.inc.php"; ?>
 
-<div class="rd-google-map margin-negative-top box-hover">
-</div>
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d965.483649673611!2d44.74125384763843!3d41.721410625777324!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe33e59b57865d3a5!2sTop+Travel!5e0!3m2!1sen!2sge!4v1522770797373" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 <footer class="page-footer text-md-left text-center">
     <div class="container">
         <div class="row">
