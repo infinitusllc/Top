45f56af40143a7947a8ac2622fc51d5aa7b8e43c
Alexander Nodia <alekonodia@gmail.com>
2018-04-05 12:08:05 +0400

    home

--- mods/login_form.mod.php
+++ mods/login_form.mod.php
@@ -10,16 +10,21 @@ if(isset($_COOKIE['e_mail']) and isset($_COOKIE['password'])
 
 <div id="login-form" class="overlay" style="z-index: 110">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <div class="overlay-content" style="background-color: gray; width: fit-content; padding: 50px; margin: auto">
-        <h4 style="margin-bottom: 15px"> შესვლა </h4>
+    <div class="overlay-content login-frm common-style">
+        <h1>შესვლა</h1>
         <form id="login_form" action="includes/login.inc.php" method="post">
-            <input type="text" name="e_mail" placeholder="e-mail" style="border: solid darkgray; background-color: floralwhite"/> <br> <br>
-            <input type="password" name="password" placeholder="password" style="border: solid darkgray; background-color: floralwhite"/> <br> <br>
-            <button type="submit" class="button sub" name="submit" style="border: solid darkgray; padding: 10px"> შესვლა</button> <br> <br>
-            <input type="checkbox" name="remember" value="true"  title="remember" style="-webkit-appearance: checkbox"/> იუზერის დამახსოვრება <br> <br>
-        </form>
-        <form action="registration.php" style="display: inline-block; margin: 5px">
-            <input type="submit" value="რეგისტრაცია" style="border: solid darkgray; padding: 10px"/>
+            <input type="text" style="width:255px" name="e_mail" placeholder="e-mail"/><br>
+            <input type="password" style="width:255px" name="password" placeholder="password"/><br>
+			<!--
+				<label>
+				  <input type='checkbox'>
+				  <span></span>
+				  მომხმარებლის დამახსოვრება
+				</label>
+				<input type="checkbox" name="remember" value="true"  title="remember"/> იუზერის დამახსოვრება<br>
+			-->
+			<button type="submit" class="button sub" name="submit"> შესვლა</button><br>
+			<a href="registration.php">მომხმარებლის რეგისტრაცია..</a>
         </form>
     </div>
 </div>
